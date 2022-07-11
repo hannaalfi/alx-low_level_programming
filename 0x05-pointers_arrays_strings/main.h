265de5117220260d632533a0f83054f25557277c
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-11 01:25:28 -0700

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
-int _putchar(0-reset_to_98.c;
+void reset_to_98(int *);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
