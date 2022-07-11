49fff4bd48144c1c42d46d17c1b815d9a7e8037e
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-11 02:02:31 -0700

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* rev_string - function that prints a string, in reverse,
+*             followed by a new line
+* @s: pointer type char
+* Return: Always 0 (Success)
+*/
+void rev_string(char *s)
+{
+	char *first = s;
+	int length = 0;
+	char tmp;
+
+	while (*s != '\0')
+	{
+		length = length + 1;
+		s = s + 1;
+	}
+	s--;
+
+	while (first <= s)
+	{
+		tmp = *first;
+		*first = *s;
+		*s = tmp;
+		first++;
+		s--;
+	}
+
+}
