22a3e8ddcb525d95b843cc58dcd57043ba24c334
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-11 01:49:45 -0700

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,16 +1,12 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-void reset_to_98(int *);
-void swap_int(int *, int *);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-
-#endif
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _putchar(char c);
