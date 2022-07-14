ae7403778aae1c9586a0ef76f8627754075472a6
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-13 23:09:19 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,4 @@ void reverse_array(int *a, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
+void print_buffer(char *b, int size);
