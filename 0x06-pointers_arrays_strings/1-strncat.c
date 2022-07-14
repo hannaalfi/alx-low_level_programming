312ae32cad6d7dce6ba3063e89f4eb1feb9b24ea
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-13 22:43:09 -0700

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* length - length str
+* @dest: - pointer
+* Return: length (Success)
+*/
+int length(char *dest)
+{
+	return ((*dest != '\0') ? 1 + length(dest + 1) : 0);
+}
+/**
+* _strncat - ry point
+* @dest: - pointer
+* @src: - pointer
+* @n: - pointer
+* Return: Always 0 (Success)
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	const unsigned int log = length(dest);
+
+	for (i = 0; i < n; i++)
+		*(dest + log + i) = *(src + i);
+	return (dest);
+}
