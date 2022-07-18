8e0b26344354a20393ab57effb630257e6ba9599
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-18 13:41:36 -0700

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_chessboard - Entry point
+* @a: char
+* Return: Always 0 (Success)
+*/
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+	const unsigned int TOP = 8;
+
+
+	for (i = 0; i < TOP; i++)
+	{
+		for (j = 0; j < TOP; j++)
+			_putchar(*(*(a + i) + j));
+		_putchar('\n');
+	}
+}
