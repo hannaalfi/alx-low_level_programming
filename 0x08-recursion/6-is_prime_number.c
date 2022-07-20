765cb0e4e0e069a4b7403ee27e1cdca0f2b7346e
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-20 11:21:55 -0700

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* prime - point
+* @n: double
+* @divisor: double
+* Return: Always 0 (Success)
+*/
+int prime(int n, int divisor)
+{
+	if (n / 2 < divisor)
+		return (1);
+	else if (n % divisor == 0)
+		return (0);
+	return (prime(n, divisor + 1));
+
+}
+/**
+* is_prime_number -  point
+* @n: char
+* Return: Always 0 (Success)
+*/
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime(n, 2));
+}
