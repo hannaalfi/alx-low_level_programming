d6eb43691e88b667c74208f2357d38fce6ab1007
Hanna <102633359+hannaalfi@users.noreply.github.com>
2022-07-20 11:20:15 -0700

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _print_rev_recursion - Entry point
+* @s: char|
+* Return: Always 0 (Success)
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
